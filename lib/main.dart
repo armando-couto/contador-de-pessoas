240c226e0e000fd0fc208c31e9011c62d987a13d
Armando Couto <coutoarmando@gmail.com>
2019-07-05 16:01:49 -0300

    Commit inicial com o projeto modelo.

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "App do Armando",
+      home: Column(
+        children: <Widget>[Text("Pessoas: 0", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))],
+      )));
+}
